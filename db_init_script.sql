9e8bc733404f0aed3655a35811a7f903addc9e80
Roniel Valdez <roniel_valdez@outlook.com>
2022-07-20 15:55:27 -0400

    infra: update db init file

--- db_init_script.sql
+++ db_init_script.sql
@@ -3,3 +3,18 @@ DROP TABLE IF EXISTS daily_app_sums;
 DROP TABLE IF EXISTS todays_app_sums;
 CREATE TABLE relay_counts (
   id INT GENERATED ALWAYS AS IDENTITY,
+  application VARCHAR NOT NULL,
+  count bigint NOT NULL,
+  time TIMESTAMPTZ
+);
+CREATE TABLE daily_app_sums (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  application VARCHAR NOT NULL,
+  count bigint NOT NULL,
+  time TIMESTAMPTZ
+);
+CREATE TABLE todays_app_sums (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  application VARCHAR NOT NULL,
+  count bigint NOT NULL
+);
