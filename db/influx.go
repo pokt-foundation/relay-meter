dd71002f3d3c803a5b53d158d3fb22cfbb43ee12
Pascal van Leeuwen <pascal@pokt.network>
2022-12-06 13:57:01 -0800

    chore: remove changes in db/influx.go

--- db/influx.go
+++ db/influx.go
@@ -57,7 +57,7 @@ func (i *influxDB) DailyCounts(from, to time.Time) (map[time.Time]map[string]api
 
 	dailyCounts := make(map[time.Time]map[string]api.RelayCounts)
 	// TODO: the influx doc seems to have a bug when describing the 'stop' parameter of range function,
-	//	i.e. it says "Results exclude rows with _time values that match the specified starzt time.", likely meant to say 'stop time'
+	//	i.e. it says "Results exclude rows with _time values that match the specified start time.", likely meant to say 'stop time'
 	//	https://docs.influxdata.com/flux/v0.x/stdlib/universe/range/
 	// 	--> this needs verification to make sure we do not double-count the last second of each day
 
@@ -135,7 +135,7 @@ func (i *influxDB) TodaysCounts() (map[string]api.RelayCounts, error) {
 	  |> keep(columns: ["applicationPublicKey", "result", "_value"])
 	  |> group(columns: ["applicationPublicKey", "result"])
 	  |> sum()`
-	fluxQuery := fmt.Sprintf(queryString, i.Options.CurrentBucket, startOfDay(time.Now().UTC()).Format(time.RFC3339))
+	fluxQuery := fmt.Sprintf(queryString, i.Options.CurrentBucket, startOfDay(time.Now()).Format(time.RFC3339))
 
 	result, err := queryAPI.Query(context.Background(), fluxQuery)
 	if err != nil {
@@ -254,11 +254,11 @@ func (i *influxDB) TodaysLatency() (map[string][]api.Latency, error) {
 	latencies := make(map[string][]api.Latency)
 
 	// TODO: send queries in parallel
-	oneDayAgo := time.Now().AddDate(0, 0, -1).Format(time.RFC3339)
+	oneDayAgo := time.Now().Add(-time.Hour * 24).Format(time.RFC3339)
 
 	queryString := `from(bucket: %q)
-	|> range(start: %s)
-	|> filter(fn: (r) => r["_measurement"] == "relay")
+	  |> range(start: %s)
+	  |> filter(fn: (r) => r["_measurement"] == "relay")
 	  |> filter(fn: (r) => r["_field"] == "elapsedTime")
 	  |> group(columns: ["host", "applicationPublicKey", "region", "result", "method"])
 	  |> keep(columns: ["applicationPublicKey", "_time", "_value"])
