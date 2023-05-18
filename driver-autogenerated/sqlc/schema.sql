c75bd8197c68b0498782e91277837c55681fdf63
Pablo Ocampo <pablojocampogo@gmail.com>
2023-05-18 13:05:27 -0400

    feat: add action to upload dockers for testing

--- driver-autogenerated/sqlc/schema.sql
+++ driver-autogenerated/sqlc/schema.sql
@@ -1,3 +1,45 @@
+CREATE TABLE relay_counts (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  origin VARCHAR NOT NULL,
+  application VARCHAR,
+  count bigint,
+  count_success INT,
+  count_failure INT,
+  time TIMESTAMPTZ
+);
+
+CREATE TABLE todays_relay_counts (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  time TIMESTAMPTZ,
+  application VARCHAR,
+  origin VARCHAR NOT NULL,
+  count_success INT,
+  count_failure INT,
+  count bigint
+);
+
+CREATE TABLE daily_app_sums (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  application VARCHAR NOT NULL,
+  count_success bigint NOT NULL,
+  count_failure bigint NOT NULL,
+  time TIMESTAMPTZ
+);
+
+CREATE TABLE todays_app_sums (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  application VARCHAR NOT NULL,
+  count_success bigint NOT NULL,
+  count_failure bigint NOT NULL
+);
+
+CREATE TABLE todays_app_latencies (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  application VARCHAR NOT NULL,
+  time VARCHAR NOT NULL,
+  latency DECIMAL NOT NULL
+);
+
 CREATE SEQUENCE success_seq START 1;
 CREATE SEQUENCE error_seq START 1;
 
